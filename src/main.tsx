d4568f050d294bc1b41b6d10043ffa4aa004ce86
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-03-23 16:28:51 -0300

    build: add chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/global.css";
 
+import { ChakraProvider } from "@chakra-ui/react";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<ChakraProvider>
+			<App />
+		</ChakraProvider>
 	</React.StrictMode>
 );
